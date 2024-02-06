2282bcad595a16682cdc87944d933d8ec7421e8e
okashishkumar <ashish@okindia.com>
2024-02-06 15:28:09 +0530

    code fromate

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,34 +1,32 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
+
+const DbConnection = () => {
+
+    try {
 
 
+        const db_url = process.env.DB_URL;
+        mongoose.connect(db_url);
 
-const DbConnection=()=>{
+        // if connected successfully
+        mongoose.connection.on("connected", () => {
+            console.log(" database connected successfully")
+        })
 
-    try{
-
-
-const db_url=process.env.DB_URL;
-mongoose.connect(db_url);
-
-// if connected successfully
-mongoose.connection.on("connected",()=>{
-    console.log(" database connected successfully")
-})
-
-// if get error during db connection
-mongoose.connection.on("error",(error)=>{
-    console.log("Get error during connection to db",error)
-})
+        // if get error during db connection
+        mongoose.connection.on("error", (error) => {
+            console.log("Get error during connection to db", error)
+        })
 
 
     }
-    catch(error){
+    catch (error) {
         return {
-            "Message":"error duging db connection",
-            "info":error
+            "Message": "error duging db connection",
+            "info": error
         }
     }
 
 }
 
-module.exports=DbConnection;
+module.exports = DbConnection;
